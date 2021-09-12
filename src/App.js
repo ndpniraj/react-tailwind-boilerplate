8240f5ead46933c1b4f22498bfd45557dda88162
ndpniraj <ndpniraj@gmail.com>
2021-09-12 13:08:42 +0530

    react and tailwind boilerplate according to tailwind docs

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="w-screen h-screen flex items-center justify-center">
+      <p className="text-gray-500 text-2xl">React Tailwind</p>
+    </div>
+  );
+}
+
+export default App;
